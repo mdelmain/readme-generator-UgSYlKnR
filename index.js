0c535a007ced136b9a884ebd3638f56015327654
Mandy Delmaine <mandydelmaine@gmail.com>
2023-03-26 20:49:13 -0400

    updated questions list

--- index.js
+++ index.js
@@ -2,7 +2,54 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'editor',
+        name: 'description',
+        message: 'Enter a project description.',
+      },
+      {
+        type: 'editor',
+        name: 'installation',
+        message: 'Enter the installation instructions.',
+      },
+      {
+        type: 'editor',
+        name: 'usage',
+        message: 'Enter instructions and examples for use.',
+      },
+      {
+        type: 'editor',
+        name: 'contribution',
+        message: 'Enter contribution guidelines.',
+      },
+      {
+          type: 'editor',
+          name: 'test',
+          message: 'Enter testing instructions.',
+        },
+      {
+          type: 'list',
+          name: 'license',
+          message: 'Select a license.',
+          choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT', 'None'],
+        },
+      {
+          type: 'input',
+          name: 'username',
+          message: 'Enter your GitHub username.',
+        },
+      {
+          type: 'input',
+          name: 'email',
+          message: 'Enter your email address.',
+        },
+    ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
