f32b6577712bcec29c71f87a80ca192860a2f4d8
Mandy Delmaine <mandydelmaine@gmail.com>
2023-03-27 16:48:50 -0400

    added function to render license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,24 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
+// function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if ( license == "None") {
+        return "";
+    } else {
+        return ` ## License
+    
+    ${license}`
+    }
+    }
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
@@ -28,6 +36,7 @@ ${data.installation}
 
 ${data.usage}
 
+${renderLicenseSection(data.license)}
 
 ## Contributing
 
@@ -43,9 +52,6 @@ If there are additional questions, please reach out to me  here:
 
 * GitHub: https://github.com/${data.username}
 * Email: ${data.email}
-
-
-  
 `;
 }
 
