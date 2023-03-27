b67f6f4f2b8484e8590ae2ce373c6a356dcb14c9
Mandy Delmaine <mandydelmaine@gmail.com>
2023-03-27 17:12:07 -0400

    updated functions to render license badge and link.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,30 @@
 // function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if ( license == "None") {
+        return "";
+    } else if ( license == "Apache License 2.0") {
+        return `[![License: Apache v2](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](${renderLicenseLink(license)})`;
+    } else if ( license == "GNU General Public License v3.0") {
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](${renderLicenseLink(license)})`;
+    } else if ( license == "MIT") {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](${renderLicenseLink(license)})`;
+    }      
+}   
 
 // function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if ( license == "None") {
+        return "";
+    } else if ( license == "Apache License 2.0") {
+        return "https://opensource.org/licenses/Apache-2.0";
+    } else if ( license == "GNU General Public License v3.0") {
+        return "https://www.gnu.org/licenses/gpl-3.0";
+    } else if ( license == "MIT") {
+        return "https://opensource.org/licenses/MIT";
+    }
+} 
 
 // function that returns the license section of README
 // If there is no license, return an empty string
@@ -20,9 +40,12 @@ function renderLicenseSection(license) {
 
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  
+return `# ${data.title}
 
-  ## Description
+${renderLicenseBadge(data.license)}
+
+## Description
   
 ${data.description}
   
